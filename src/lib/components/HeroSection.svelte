c110b17285dd763e6d0d867004eafcd6fefa87d3
Eramood <122992858+Eramood@users.noreply.github.com>
2025-02-06 18:32:59 +0100

    starting a landing page the hero section

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,28 @@
+<script>
+  import bookCover from "$assets/book_cover.png";
+  import phoneCover from "$assets/phone_cover.png";
+</script>
+
+<section class="Hero">
+  <div class="hero-showcase text-center bold">
+    <h1 class="purple">MOVE TO</h1>
+    <h1 class="purple mb-xs">SPAIN</h1>
+    <h4 class="italic semi-bold">
+      There's no need to make the same mistakes I made.
+    </h4>
+    <img class="book-cover" src={bookCover} alt="" />
+    <img class="phone-cover" src={phoneCover} alt="" />
+  </div>
+
+  <div class="hero-tct white text-center">
+    <h1>THIS EBOOK WILL</h1>
+    <h1>SAVE YOU MONEY,</h1>
+    <h1 class="mb-m">TIME & NERVS</h1>
+    <p class="light-grey">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+    </p>
+  </div>
+</section>
