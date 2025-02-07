f8c06eb27603d7aa3d0cf8ed004fd46bdf8ca327
Eramood <122992858+Eramood@users.noreply.github.com>
2025-02-07 16:19:34 +0100

    Create a reusable button and The chapter Preview

---
+++ src/lib/components/ChapterPreview.svelte
@@ -0,0 +1,74 @@
+<script>
+</script>
+
+<section class="chapter-preview default-margin">
+  <h2 class="mb-l">What you're getting</h2>
+    <div class="chapter-container">
+        <ul>
+        <li>
+            <button
+            class="chapter-title"
+            aria-controls="chapter-info-1"
+            aria-expanded="true"
+            >
+            <h3>Chapter 1: Navigating the Spanish bereaucracy</h3>
+            </button>
+        </li>
+        </ul>
+        <div class="chapter-info">
+            <h3 class="chapter-strapline italic mb-s">
+                understanting Visas,Residency, and Legal Essentials
+            </h3>
+            <p>
+                Moving to Spain involves navigating a labyrinth of bureaucratic
+                processes that can feel overwhelming at first. This chapter breaks down
+                the essential steps required to secure your visa, establish residency,
+                and handle other legal formalities. Whether you’re planning a short-term
+                stay or seeking permanent residency, understanding Spain’s legal
+                framework is crucial. We’ll explore the different types of visas
+                available, from student to work visas, and provide tips on how to
+                streamline your application process. Additionally, this chapter covers
+                essential documentation, such as obtaining a Número de Identidad de
+                Extranjero (NIE), which is your key to unlocking many services in Spain.
+            </p>
+        </div>
+    </div>
+</section>
+
+<style>
+  .chapter-preview {
+    padding: 80px;
+    margin-left: 12vw;
+    margin-right: 20vw;
+    max-width: 1150px;
+  }
+  .chapter-container {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .chapter-container ul {
+    width: 40%;
+  }
+
+  .chapter-info {
+    width: 55%;
+  }
+
+  .chapter-title {
+    border-bottom: 1px solid grey;
+    width: 100%;
+    display: block;
+    padding: 12px;
+    text-align: left;
+  }
+
+  .selected-chapter-title {
+    background-color: black;
+    border: none;
+    color: white;
+    box-shadow:
+      0 4px 6px rgba(0, 0, 0, 0.1),
+      0 1px 3px rgba(0, 0, 0, 0.08);
+  }
+</style>
